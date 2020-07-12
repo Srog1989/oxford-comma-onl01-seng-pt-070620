e1aaf10854bb4c9d14d840e1b57bb5e91e62f907
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-12 19:43:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
  if array.length==1
   array.join
- elsif array.length ==2 
+ elsif array.length==2 
   array.join("and ")
  elsif array.length>= 3
-  array[-1].prepend " and "
+  array[-1].prepend "and "
   array.join(", ")
  end
 end 
