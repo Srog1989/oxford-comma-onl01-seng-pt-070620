4c78d44d97436bb5c32244c2552592d356eba50f
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-12 19:44:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
  if array.length==1
   array.join
  elsif array.length==2 
-  array.join("and ")
- elsif array.length>= 3
+  array.join(" and ")
+ elsif array.length>=3
   array[-1].prepend "and "
   array.join(", ")
  end
